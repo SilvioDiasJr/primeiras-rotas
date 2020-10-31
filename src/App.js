0ae5e4bd2ea6fc87c6ca91dc34503db42829895e
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-31 02:18:51 -0300

    modificar importação de rota

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import './App.css';
+
+import Routes from './components/Routes';
+
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-
+      <Routes />
     </div>
   );
 }
